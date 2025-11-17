256ee8e001b9d6e402d3aad1a7356317cadeb80b
김혜림 <hyerim218@naver.com>
2025-11-17 13:13:38 +0900

    feat: 완료컨테이너 구현 #15

---
+++ pome/src/components/DoneTrash/DoneTrash.style.tsx
@@ -0,0 +1,23 @@
+import styled from "@emotion/styled";
+export const DoneTrashContainer = styled.div`
+  width: 364px;
+  display: flex;
+  justify-content: space-between;
+  padding: 20px 0;
+`;
+export const DoneBox = styled.div`
+  color: #097795;
+  text-align: center;
+  leading-trim: both;
+  text-edge: cap;
+  font-family: "Pretendard Variable";
+  font-size: 15.102px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  width: 74px;
+  height: 24.918px;
+  flex-shrink: 0;
+  border-radius: 22.653px;
+  background: rgba(178, 232, 255, 0.5);
+`;
