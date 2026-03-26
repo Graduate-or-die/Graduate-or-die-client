3ca1f752cc5e33d78456ef42b1112b9d72cbe440
김혜림 <hyerim218@naver.com>
2026-03-27 01:14:41 +0900

    fix: 코드 수정

--- pome/src/components/RequestItem/RequestItem.tsx
+++ pome/src/components/RequestItem/RequestItem.tsx
@@ -5,41 +5,54 @@ import { RequestProfile } from "../../assets";
 type RequestItemProps = {
   userId: number;
   nickname: string;
-  job: string;
-  isMatched: boolean;
+  profileImage: string | null;
+  introduction: string;
   onReject: (userId: number) => void;
   onAccept: (profile: {
     userId: number;
     nickname: string;
-    isMatched: boolean;
   }) => void;
 };
 
 export default function RequestItem({
   userId,
   nickname,
-  job,
-  isMatched,
+  profileImage,
+  introduction,
   onReject,
   onAccept,
 }: RequestItemProps) {
   return (
-    <>
-      <S.RequestContainer>
-        <S.ProfileBox>
+    <S.RequestContainer>
+      <S.ProfileBox>
+        {profileImage ? (
+          <img src={profileImage} alt="profile" />
+        ) : (
           <RequestProfile />
-        </S.ProfileBox>
-        <S.ItemBox>
-          <S.NameBox>{nickname}</S.NameBox>
-          <S.SummaryBox>{job}</S.SummaryBox>
-        </S.ItemBox>
-        <S.SelectBox>
-          <S.RejectBox onClick={() => onReject(userId)}>거절</S.RejectBox>
-          <S.AllowBox onClick={() => onAccept({ userId, nickname, isMatched })}>
-            수락
-          </S.AllowBox>
-        </S.SelectBox>
-      </S.RequestContainer>
-    </>
+        )}
+      </S.ProfileBox>
+
+      <S.ItemBox>
+        <S.NameBox>{nickname}</S.NameBox>
+        <S.SummaryBox>{introduction}</S.SummaryBox>
+      </S.ItemBox>
+
+      <S.SelectBox>
+        <S.RejectBox onClick={() => onReject(userId)}>
+          거절
+        </S.RejectBox>
+
+        <S.AllowBox
+          onClick={() =>
+            onAccept({
+              userId,
+              nickname,
+            })
+          }
+        >
+          수락
+        </S.AllowBox>
+      </S.SelectBox>
+    </S.RequestContainer>
   );
 }
